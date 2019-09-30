2cee1657a55c7aa3ba972dfdcccdd7c5cbbfe630
unknown <IBMADMIN@DESKTOP-U3I82D4.yellowdc.com>
2019-09-30 17:18:37 +0530

    kttrsdghju98rer

---
+++ src/main/java/m_p/pyt/App.java
@@ -0,0 +1,13 @@
+package m_p.pyt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/resources/my.py
@@ -0,0 +1,5 @@
+# coding=UTF-8
+import os,io
+cur_path = os.getcwd() #should be the src/main/resources
+with io.open(cur_path+"/tempfile", mode='w') as the_file:
+    the_file.write(u'hello maven'
